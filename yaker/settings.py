ce8cff53a8a1b0ca0907629c0dae718aa5b4cb44
Drakirus <pierrescalade@gmail.com>
2017-02-27 18:59:41 +0100

    bu

--- yaker/settings.py
+++ yaker/settings.py
@@ -24,11 +24,18 @@ SECRET_KEY = '11111111111111111111111111111111111111111111111111'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_ROOT = os.path.join(os.path.abspath(
+    os.path.join(BASE_DIR, 'yaker-client-angularJs')), '')
+
+STATIC_URL = '/static/'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'yaker-client-angularJs')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -150,8 +157,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
--- yaker/urls.py
+++ yaker/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^users/', include('users.urls')),
     url(r'^game/', include('game.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^play/', TemplateView.as_view(template_name='index.html'), name='play-client-view'),
 ]
