37cb8e497757d0efb67924483860bf096e34934a
Pierre CHAMPION <pierrescalade@gmail.com>
2017-03-16 23:10:54 +0000

    Mettre à jour 'server/yaker/settings.py'

--- server/yaker/settings.py
+++ server/yaker/settings.py
@@ -23,7 +23,7 @@ redis_host = os.environ.get('REDIS_HOST', 'localhost')
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '89144099147032575824659566537409'
+SECRET_KEY = 'coucou'
 
 if os.environ.get('REDIS_HOST', None):
     DEBUG = False
