d82896462a18fdc516da28f06abe665af80d8397
Drakirus <pierrescalade@gmail.com>
2017-03-19 17:13:42 +0100

    Some more inforamation on admin panel best player of game, best score users

--- server/game/admin.py
+++ server/game/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
 from .models import Game, Save
+
+from django.utils.html import format_html
+
 admin.autodiscover()
 
-admin.site.register(Game)
+#  admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    """Display the users Save related to the game"""
+
+
+    def game_total_played(self, g):
+        try:
+            return Save.objects.filter(game_id=g).count()
+        except:
+            return ''
+
+
+    def game_best_player(self, g):
+        try:
+            user_save = Save.objects.filter(game_id=g).order_by('-score').first()
+            return format_html(
+                '<span style="color:#80BFFF;">{}</span><br/>{} Pts',
+                user_save.user.username,
+                user_save.score
+            )
+        except:
+            return ''
+
+    model = Game
+
+    game_total_played.short_description = 'Total Played'
+    game_best_player.short_description = 'Best Player'
+    list_display = ('id', 'game_set','game_total_played',
+                    'game_best_player')
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Save)
--- server/users/admin.py
+++ server/users/admin.py
@@ -1,5 +1,70 @@
 from django.contrib import admin
+
 from .models import Followership
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from game.models import Save
+
+from .users_games_stats import *
+
+from django.utils.html import format_html
+
 admin.autodiscover()
 
 admin.site.register(Followership)
+
+class CustomUserAdmin(UserAdmin):
+
+    """Some more inforamtion about users games in admin panel"""
+
+    def user_avg(self, u):
+        try:
+            return get_avg_score(u)
+        except:
+            return ''
+
+    def user_max(self, u):
+        try:
+            max_s = Save.objects.filter(user=u).order_by('-score').\
+                values('score','game_id').first()
+
+            return format_html(
+                '<span style="color:#80BFFF;">{}</span><br/>{}',
+                str(max_s['score']) + " Pts",
+                "game id :" + str(max_s['game_id'])
+            )
+        except:
+            return ''
+
+    def user_last_played(self, u):
+        try:
+            last_p = Save.objects.filter(user=u).order_by('-date').\
+                values('score','date','game_id').first()
+            return format_html(
+                '<span style="color:#80BFFF;">{}</span><br/>{}',
+                last_p['date'].strftime('%m/%d/%Y') + last_p['date'].strftime(" at %H:%M"),
+                str(last_p['score']) + "  Pts on game :" + str(last_p['game_id'])
+            )
+
+        except:
+            return ''
+
+    def user_played_count(self, u):
+        try:
+            count = Save.objects.filter(user=u).count()
+            return count
+
+        except:
+            return ''
+
+    user_avg.short_description = 'Avg Score'
+    user_played_count.short_description = 'Played Games'
+    user_last_played.short_description = 'Last Game Played'
+    user_max.short_description = 'Max Score'
+
+    list_display = ("username", "email", "date_joined","is_staff",
+                    'user_avg','user_max','user_last_played','user_played_count',)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
